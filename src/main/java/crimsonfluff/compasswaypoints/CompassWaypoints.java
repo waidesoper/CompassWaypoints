09d2c9f37b0b92745762a6aa8749beebf7a95b0b
waidesoper <waide.soper@gmail.com>
2022-01-08 12:00:22 +1100

    moved the predicates to the best place. Used code from natures compass

--- src/main/java/crimsonfluff/compasswaypoints/CompassWaypoints.java
+++ src/main/java/crimsonfluff/compasswaypoints/CompassWaypoints.java
@@ -18,57 +18,10 @@ import org.apache.logging.log4j.Logger;
 public class CompassWaypoints implements ModInitializer {
     public static final String MOD_ID = "compasswaypoints";
     public static final Logger LOGGER = LogManager.getLogger();
-    private double prevAngle = 0.0D;
-    private double prevWobble = 0.0D;
-    private long prevWorldTime = 0L;
+
+
     @Override
     public void onInitialize() {
         initItems.register();
-       FabricModelPredicateProviderRegistry.register(initItems.WAYPOINT_COMPASS_ITEM, new Identifier("angle"), (stack, world, livingEntity,x) -> {
-            boolean isLiving = livingEntity != null;
-
-            if (!isLiving && !stack.isInFrame() || !stack.hasNbt() || !stack.getNbt().contains(WaypointCompassItem.LODESTONE_POS_KEY)) return 0;
-
-            Entity entity = isLiving ? livingEntity : stack.getFrame();
-
-            if (world == null) world = (ClientWorld) entity.world;
-
-            BlockPos blockPos = NbtHelper.toBlockPos(stack.getNbt().getCompound(WaypointCompassItem.LODESTONE_POS_KEY));
-
-            double angle;
-            double entityAngle = isLiving ? entity.getEyeY() : getFrameAngle((ItemFrameEntity) entity);
-
-            entityAngle /= 360.0D;
-            entityAngle = MathHelper.floorMod(entityAngle, 1.0D);
-            double posAngle = getPosToAngle(blockPos, entity);
-            posAngle /= Math.PI * 2D;
-            angle = 0.5D - (entityAngle - 0.25D - posAngle);
-
-            if (isLiving) angle = wobble(world, angle);
-
-            return MathHelper.floorMod((float) angle, 1.0F);
-        });
-    }
-    private double wobble(World world, double angle) {
-        long worldTime = world.getTime();
-
-        if (worldTime != prevWorldTime) {
-            prevWorldTime = worldTime;
-            double angleDifference = angle - prevAngle;
-            angleDifference = MathHelper.floorMod(angleDifference + 0.5D, 1.0D) - 0.5D;
-
-            prevWobble += angleDifference * 0.1D;
-            prevWobble *= 0.8D;
-            prevAngle = MathHelper.floorMod(prevAngle + prevWobble, 1.0D);
-        }
-
-        return prevAngle;
-    }
-    private double getFrameAngle(ItemFrameEntity entity) {
-        return MathHelper.wrapDegrees(180 + entity.getHorizontalFacing().getOffsetY() * 90);
-    }
-
-    private double getPosToAngle(BlockPos pos, Entity entity) {
-        return Math.atan2(pos.getZ() - entity.getZ(), pos.getX() - entity.getX());
     }
 }
--- src/main/java/crimsonfluff/compasswaypoints/client/CompassWaypointsClient.java
+++ src/main/java/crimsonfluff/compasswaypoints/client/CompassWaypointsClient.java
@@ -1,14 +1,84 @@
 package crimsonfluff.compasswaypoints.client;
 
+import crimsonfluff.compasswaypoints.init.initItems;
+import crimsonfluff.compasswaypoints.item.WaypointCompassItem;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
+import net.fabricmc.fabric.api.object.builder.v1.client.model.FabricModelPredicateProviderRegistry;
+import net.minecraft.client.item.UnclampedModelPredicateProvider;
+import net.minecraft.client.world.ClientWorld;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.decoration.ItemFrameEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NbtHelper;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.MathHelper;
+import net.minecraft.world.World;
 
 @Environment(EnvType.CLIENT)
 public class CompassWaypointsClient implements ClientModInitializer {
 
     @Override
     public void onInitializeClient() {
+        FabricModelPredicateProviderRegistry.register(initItems.WAYPOINT_COMPASS_ITEM, new Identifier("angle"), new UnclampedModelPredicateProvider() {
+            private double rotation;
+            private double rota;
+            private long lastUpdateTick;
 
+            @Override
+            public float unclampedCall(ItemStack stack, ClientWorld world, LivingEntity entityLiving, int seed) {
+                if (entityLiving == null && !stack.isInFrame()) {
+                    return 0.0F;
+                } else {
+                    final boolean entityExists = entityLiving != null;
+                    final Entity entity = (Entity) (entityExists ? entityLiving : stack.getFrame());
+                    if (world == null && entity.world instanceof ClientWorld) {
+                        world = (ClientWorld) entity.world;
+                    }
+
+                    double rotation = entityExists ? (double) entity.getYaw() : getFrameRotation((ItemFrameEntity) entity);
+                    rotation = rotation % 360.0D;
+                    double adjusted = Math.PI - ((rotation - 90.0D) * 0.01745329238474369D - getAngle(world, entity, stack));
+
+                    if (entityExists) {
+                        adjusted = wobble(world, adjusted);
+                    }
+
+                    final float f = (float) (adjusted / (Math.PI * 2D));
+                    return MathHelper.floorMod(f, 1.0F);
+                }
+            }
+
+            private double wobble(ClientWorld world, double amount) {
+				if (world.getTime() != lastUpdateTick) {
+					lastUpdateTick = world.getTime();
+					double d0 = amount - rotation;
+					d0 = d0 % (Math.PI * 2D);
+					d0 = MathHelper.clamp(d0, -1.0D, 1.0D);
+					rota += d0 * 0.1D;
+					rota *= 0.8D;
+					rotation += rota;
+				}
+
+				return rotation;
+			}
+
+            private double getFrameRotation(ItemFrameEntity itemFrame) {
+                return (double) MathHelper.wrapDegrees(180 + itemFrame.getHorizontalFacing().getHorizontal() * 90);
+            }
+
+            private double getAngle(ClientWorld world, Entity entity, ItemStack stack) {
+                if (stack.getItem() == initItems.WAYPOINT_COMPASS_ITEM) {
+                    WaypointCompassItem compassItem = (WaypointCompassItem) stack.getItem();
+                    BlockPos pos = NbtHelper.toBlockPos(stack.getNbt().getCompound(WaypointCompassItem.LODESTONE_POS_KEY));
+                    return Math.atan2((double) pos.getZ() - entity.getPos().z, (double) pos.getX() - entity.getPos().x);
+                }
+                return 0.0D;
+            }
+
+        });
     }
 }
